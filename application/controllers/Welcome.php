0f9e2030f960874c3a565565da685935c3e08e76
souha amor <samor001@seattlecentral.edu>
2016-04-12 15:40:07 -0700

    add wordpress like header , footer files in our app

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -6,10 +6,13 @@ class Welcome extends CI_Controller {
 
 	public function index()
 	{ //associative array of data to pass to view
-        $data['title']= "My unique title tag";
-        $data['page_id']="My clever page ID";
-        $data['guts']="<p>I'am the creamy filling inside </p>";
+        $data['title']= "My unique title tag, now inside header";
+        $data['page_id']="My clever page ID, in header";
+        $data['guts']="<p>I'am still the creamy filling inside </p>";
         //data is passed as second param in view creation
-		$this->load->view('welcome_message', $data);
+		$this->load->view('templates/header', $data);
+        $this->load->view('welcome_message', $data);
+        $this->load->view('templates/footer', $data);
+        
 	}
 }
